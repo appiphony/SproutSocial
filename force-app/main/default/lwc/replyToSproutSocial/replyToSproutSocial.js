f93ffadcbce3e27dc882619ab01b73db777df023
basco-appiphony <basco@appiphony.com>
2022-07-19 11:53:56 -0500

    added component with correct spelling

---
+++ force-app/main/default/lwc/replyToSproutSocial/replyToSproutSocial.js
@@ -0,0 +1,8 @@
+import { LightningElement } from 'lwc';
+import sproutSocialIcon from '@salesforce/resourceUrl/sproutSocialIcon';
+
+
+export default class ReplyToSproutSocial extends LightningElement {
+    sproutSocialIcon = sproutSocialIcon;
+    
+}
