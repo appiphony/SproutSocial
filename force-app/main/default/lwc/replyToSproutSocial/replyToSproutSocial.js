bcbec5b2bde3627c4b06c2713040743fd24f04d1
basco-appiphony <basco@appiphony.com>
2022-07-28 08:51:45 -0500

    tweak code for classic and added condition to check for perm set and completion of setup

--- force-app/main/default/lwc/replyToSproutSocial/replyToSproutSocial.js
+++ force-app/main/default/lwc/replyToSproutSocial/replyToSproutSocial.js
@@ -1,8 +1,16 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, track } from 'lwc';
 import sproutSocialIcon from '@salesforce/resourceUrl/sproutSocialIcon';
+import sproutSocialEmptyState from '@salesforce/resourceUrl/sproutSocialEmptyState';
 
 
 export default class ReplyToSproutSocial extends LightningElement {
     sproutSocialIcon = sproutSocialIcon;
+    sproutSocialEmptyState = sproutSocialEmptyState;
+    @track isClassic = false;
+    @track hasError = true;
+    @track setupComplete = false; 
     
+    get errorMessage() {
+        return !this.setupComplete ? 'Complete the Sprout Social Setup Assistant to use this component.' : 'This user does not have permission to use this component. Please contact your admin.';
+    }
 }
